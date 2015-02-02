1834c61a59833b4869e58b9f655ecc422b0927a7
Shah <shah@Shahs-Mac-Pro.local>
2015-02-02 15:11:44 -0800

    Initial commit
    Signed-off-by: Shah <shah@Shahs-Mac-Pro.local>

---
+++ iMuzik/iMuzik/DetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailsViewController.swift
+//  iMuzik
+//
+//  Created by Shah on 1/26/15.
+//  Copyright (c) 2015 COMP4977. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    
+    var album: Album?
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
